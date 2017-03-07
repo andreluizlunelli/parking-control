8c00dcb98cefa4dae236fd5baf33b96ef7980621
André Lunelli <to.lunelli@gmail.com>
2017-03-07 09:17:25 -0300

    commit inicial

---
+++ parking-control/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(parking_control.Startup))]
+namespace parking_control
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

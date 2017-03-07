cf1e491db28fafb3266791ac121cbb963d24196c
André Lunelli <to.lunelli@gmail.com>
2017-03-07 13:23:41 -0300

    construindo serviço para controle de entrada de veiculos

---
+++ parking-control.Tests/Service/ValidityControlTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using parking_control.Models;
+
+namespace parking_control.Tests.Service
+{
+    [TestClass]
+    class ValidityControlTest
+    {
+        [TestMethod]
+        public void SetPrice01()
+        {
+            DateTime initialDateControl = new DateTime(2015, 8, 16, 0, 0, 0);
+            DateTime finalDateControl = new DateTime(2015, 11, 15, 23, 59, 59);
+            double price = 5;
+
+            ValidityControl.SetDateControl(price, initialDateControl, finalDateControl);
+            Assert.IsTrue(, "deu ruin");
+        }
+
+        public void ReturnPrice01()
+        {
+            DateTime initialDateIn = new DateTime(2015, 9, 10, 14, 0, 0);
+            DateTime initialDateOut = new DateTime(2015, 11, 15, 16, 0, 0);
+
+            ValidityControl.GetReturnPrice();
+
+        }
+    }
+}
---
+++ parking-control/Models/ValidityControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace parking_control.Models
+{
+    public class ValidityDateControl
+    {
+        public DateTime InitialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class ValidityControl
+    {
+    }
+}
